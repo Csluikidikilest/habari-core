e2b2b8778554762c4477f206f31166aa1f4d5841
csluikidikilest <csluikidikilest@gmail.com>
2022-07-03 12:20:48 +0200

    Handle differents processes

---
+++ src/main/java/org/qazima/habari/core/library/content/MetadataContext.java
@@ -0,0 +1,38 @@
+package org.qazima.habari.core.library.content;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.apache.http.HttpStatus;
+import org.qazima.habari.core.library.configuration.Configuration;
+import org.qazima.habari.pluginsystem.interfaces.IPlugin;
+import org.qazima.habari.pluginsystem.library.Content;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class MetadataContext implements HttpHandler {
+    private Configuration configuration;
+
+    public MetadataContext(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String requestUri = exchange.getRequestURI().getPath();
+        List<IPlugin> iPlugins = configuration.getConnections().stream().filter(item -> Pattern.compile(item.getConfiguration().getUri()).matcher(requestUri).matches()).toList();
+        Content content = new Content();
+        int contentResult = HttpStatus.SC_NOT_FOUND;
+        for (IPlugin iPlugin :
+                iPlugins) {
+            contentResult = iPlugin.processMetadata(exchange, content);
+        }
+        byte[] response = content.getBody();
+        exchange.sendResponseHeaders(contentResult, response.length);
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(response);
+        outputStream.close();
+    }
+}
